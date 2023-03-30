92509e3e2620422a08ef230e9de01ff387dcd8a5
Gaël Paquien <contact@gaelpaquien.com>
2023-03-30 22:21:41 +0200

    feat:(Routes-#10-#11) GET routes to retrieve one or several products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,44 +4,30 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use App\Service\APIService;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 class ProductController extends AbstractController
 {
-    #[Route('/api/products/', name: 'product_list', methods: ['GET'])]
-    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    private APIService $apiService;
+
+    public function __construct(SerializerInterface $serializer)
     {
-        $productList = $productRepository->findAll();
+        $this->apiService = new APIService($serializer);
+    }
 
-        $circularReferenceHandler = function ($object) {
-            return $object->getId();
-        };
-
-        $jsonProductList = $serializer->serialize($productList, 'json', [
-            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => $circularReferenceHandler,
-            AbstractNormalizer::IGNORED_ATTRIBUTES => ['brand'],
-        ]);
-
-        return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
+    #[Route('/api/products/', name: 'product_list', methods: ['GET'])]
+    public function getAllProducts(ProductRepository $productRepository): JsonResponse
+    {
+        return $this->apiService->getRoutes($productRepository->findAll(), ['product:read']);
     }
 
     #[Route('/api/products/{id}', name: 'product_detail', methods: ['GET'])]
-    public function getProduct(Product $product, SerializerInterface $serializer): JsonResponse
+    public function getProduct(Product $product): JsonResponse
     {
-        $circularReferenceHandler = function ($object) {
-            return $object->getId();
-        };
-
-        $jsonProduct = $serializer->serialize($product, 'json', [
-            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => $circularReferenceHandler,
-            AbstractNormalizer::IGNORED_ATTRIBUTES => ['brand'],
-        ]);
-
-        return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
+        return $this->apiService->getRoutes($product, ['product:read']);
     }
 }

cb35fcbe35ca4b9c0793c81b7fa120463d315995
Galuss <galuss.contact@gmail.com>
2023-04-21 06:01:01 +0200

    feat:(ErrorsManagement-#27): Data validation with symfony/validator

--- src/Service/CustomerService.php
+++ src/Service/CustomerService.php
@@ -8,25 +8,34 @@ use App\Entity\Customer;
 use App\Entity\CustomerAddress;
 use App\Repository\CompanyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class CustomerService
 {
-    public function __construct(private readonly SerializerInterface $serializer, private readonly CompanyRepository $companyRepository, private readonly EntityManagerInterface $em)
+    public function __construct(private readonly SerializerInterface $serializer, private readonly CompanyRepository $companyRepository, private readonly EntityManagerInterface $em, private readonly ValidatorInterface $validator)
     {
     }
 
-    public function createCustomer(Request $request): Customer
+    public function createCustomer(Request $request): Customer | JsonResponse
     {
         // Recovers all the data that has been sent
         $content = $request->toArray();
 
         $customer = $this->serializer->deserialize($request->getContent(), Customer::class, 'json');
 
-        // Recovers the company id and sets it to the customer
-        $idCompany = $content['company_id'];
-        $customer->setCompany($this->companyRepository->find($idCompany));
+        // Recovers the company and checks if it exists
+        $companyId = $request->attributes->get('companyId');
+        $companyExists = $this->companyRepository->findOneBy(['id' => $companyId]);
+        if (!$companyExists) {
+            return new JsonResponse('Cette entreprise n\'existe pas', Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        // Sets the company to the customer
+        $customer->setCompany($this->companyRepository->find($companyId));
 
         // Recovers the address informations and sets it to the customer
         $address = new CustomerAddress();
@@ -38,6 +47,28 @@ final class CustomerService
         $address->setAddressDetails(($content['address']['address_details'] ?? null));
 
         $customer->addCustomerAddress($address);
+
+        // Validates the customer and the address
+        $customerErrors = $this->validator->validate($customer);
+        $addressErrors = $this->validator->validate($address);
+        $errors = array_merge($customerErrors->getIterator()->getArrayCopy(), $addressErrors->getIterator()->getArrayCopy());
+
+        if (count($errors) > 0) {
+            $messages = [];
+            foreach ($errors as $error) {
+                $property = $error->getPropertyPath();
+                $message = (string) $error;
+                $messages[$property] = $message;
+            }
+            $jsonResponse = json_encode($messages, JSON_PRETTY_PRINT);
+            return new JsonResponse($jsonResponse, Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        // Check if the customer already exists
+        $customerExists = $this->em->getRepository(Customer::class)->findOneBy(['email' => $customer->getEmail()]);
+        if ($customerExists) {
+            return new JsonResponse('Cet email est déjà utilisé', Response::HTTP_BAD_REQUEST, [], true);
+        }
 
         $this->em->persist($customer);
         $this->em->persist($address);
